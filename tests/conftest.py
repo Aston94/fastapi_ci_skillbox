50129b27aa6b67feaa339d70552910af1a839e93
astonxach <9041618278@mail.ru>
2024-12-19 17:18:14 +0500

    structured commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,28 @@
 import pytest
 import sys
-import json
-from sqlalchemy.testing.assertsql import assert_engine
 
 sys.path.append('../')
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
-from sqlalchemy import ForeignKey, Integer, String, ForeignKeyConstraint, Table
+
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from database import Model, RecipesListOrm, RecipeOrm, create_tables, drop_tables
+from database import Model, RecipesListOrm, RecipeOrm, create_tables, drop_tables, new_session
 from main import app
+# from sqlmodel import Session, SQLModel, create_engine
+import pytest_asyncio
 
 
-DATABASE_URL = "sqlite+aiosqlite:///test.db"
+@pytest_asyncio.fixture(name='session')
+async def session_fixture():
+    _engine = create_async_engine("sqlite+aiosqlite:///test.db")
+    _new_session = async_sessionmaker(_engine, expire_on_commit=False)
+    async with _new_session() as session:
+        yield session
 
-
-@pytest.fixture(scope="module")
-def test_app():
-    _engine = create_engine(DATABASE_URL, echo=True)
-    _new_session = sessionmaker(_engine, expire_on_commit=False)
+@pytest_asyncio.fixture(name='client')
+async def client_fixture(session):
+    async def get_session_override():
+        yield session
+    app.dependency_overrides[new_session] = get_session_override
     client = TestClient(app)
-    create_tables(_engine)
-
     yield client
-
-
-
-
-
-
+    app.dependency_overrides.clear()
